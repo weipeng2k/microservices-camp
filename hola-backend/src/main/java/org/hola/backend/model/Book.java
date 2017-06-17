754aa37381540749b71aa41b1c881862af7f64ba
weipeng2k <weipeng2k@126.com>
2017-06-17 18:27:40 +0800

    add chapter2

---
+++ hola-backend/src/main/java/org/hola/backend/model/Book.java
@@ -0,0 +1,112 @@
+package org.hola.backend.model;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Version;
+import java.util.Date;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@XmlRootElement
+public class Book implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, nullable = false)
+	private Long id;
+	@Version
+	@Column(name = "version")
+	private int version;
+
+	@Column(length = 64, name = "NAME", nullable = false)
+	private String name;
+
+	@Column(length = 32, name = "AUTHOR_NAME", nullable = false)
+	private String authorName;
+
+	@Column(name = "PUBLISH_DATE", nullable = false)
+	@Temporal(TemporalType.DATE)
+	private Date publishDate;
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(final Long id) {
+		this.id = id;
+	}
+
+	public int getVersion() {
+		return this.version;
+	}
+
+	public void setVersion(final int version) {
+		this.version = version;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Book)) {
+			return false;
+		}
+		Book other = (Book) obj;
+		if (id != null) {
+			if (!id.equals(other.id)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+
+	public Date getPublishDate() {
+		return publishDate;
+	}
+
+	public void setPublishDate(Date publishDate) {
+		this.publishDate = publishDate;
+	}
+
+	@Override
+	public String toString() {
+		String result = getClass().getSimpleName() + " ";
+		if (name != null && !name.trim().isEmpty())
+			result += "name: " + name;
+		if (authorName != null && !authorName.trim().isEmpty())
+			result += ", authorName: " + authorName;
+		return result;
+	}
+}
---
+++ hola-backend/src/main/java/org/hola/backend/rest/BookEndpoint.java
@@ -0,0 +1,116 @@
+package org.hola.backend.rest;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.OptimisticLockException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriBuilder;
+import org.hola.backend.model.Book;
+
+/**
+ * 
+ */
+@Stateless
+@Path("/books")
+public class BookEndpoint {
+	@PersistenceContext(unitName = "hola-backend-persistence-unit")
+	private EntityManager em;
+
+	@POST
+	@Consumes("application/json")
+	public Response create(Book entity) {
+		em.persist(entity);
+		return Response.created(
+				UriBuilder.fromResource(BookEndpoint.class)
+						.path(String.valueOf(entity.getId())).build()).build();
+	}
+
+	@DELETE
+	@Path("/{id:[0-9][0-9]*}")
+	public Response deleteById(@PathParam("id") Long id) {
+		Book entity = em.find(Book.class, id);
+		if (entity == null) {
+			return Response.status(Status.NOT_FOUND).build();
+		}
+		em.remove(entity);
+		return Response.noContent().build();
+	}
+
+	@GET
+	@Path("/{id:[0-9][0-9]*}")
+	@Produces("application/json")
+	public Response findById(@PathParam("id") Long id) {
+		TypedQuery<Book> findByIdQuery = em
+				.createQuery(
+						"SELECT DISTINCT b FROM Book b WHERE b.id = :entityId ORDER BY b.id",
+						Book.class);
+		findByIdQuery.setParameter("entityId", id);
+		Book entity;
+		try {
+			entity = findByIdQuery.getSingleResult();
+		} catch (NoResultException nre) {
+			entity = null;
+		}
+		if (entity == null) {
+			return Response.status(Status.NOT_FOUND).build();
+		}
+		return Response.ok(entity).build();
+	}
+
+	@GET
+	@Produces("application/json")
+	public List<Book> listAll(@QueryParam("start") Integer startPosition,
+			@QueryParam("max") Integer maxResult) {
+		TypedQuery<Book> findAllQuery = em.createQuery(
+				"SELECT DISTINCT b FROM Book b ORDER BY b.id", Book.class);
+		if (startPosition != null) {
+			findAllQuery.setFirstResult(startPosition);
+		}
+		if (maxResult != null) {
+			findAllQuery.setMaxResults(maxResult);
+		}
+		final List<Book> results = findAllQuery.getResultList();
+		return results;
+	}
+
+	@PUT
+	@Path("/{id:[0-9][0-9]*}")
+	@Consumes("application/json")
+	public Response update(@PathParam("id") Long id, Book entity) {
+		if (entity == null) {
+			return Response.status(Status.BAD_REQUEST).build();
+		}
+		if (id == null) {
+			return Response.status(Status.BAD_REQUEST).build();
+		}
+		if (!id.equals(entity.getId())) {
+			return Response.status(Status.CONFLICT).entity(entity).build();
+		}
+		if (em.find(Book.class, id) == null) {
+			return Response.status(Status.NOT_FOUND).build();
+		}
+		try {
+			entity = em.merge(entity);
+		} catch (OptimisticLockException e) {
+			return Response.status(Response.Status.CONFLICT)
+					.entity(e.getEntity()).build();
+		}
+
+		return Response.noContent().build();
+	}
+}
