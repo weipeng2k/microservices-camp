855ed81fcedb407a343dad531bfcdb7067a38fb3
weipeng2k <weipeng2k@126.com>
2017-06-17 17:54:54 +0800

    delete hola-backend

--- hola-backend/src/main/java/com/murdock/examples/backend/rest/RestApplication.java
+++
@@ -1,8 +0,0 @@
-package com.murdock.examples.backend.rest;
-
-import javax.ws.rs.core.Application;
-import javax.ws.rs.ApplicationPath;
-
-@ApplicationPath("/rest")
-public class RestApplication extends Application {
-}
